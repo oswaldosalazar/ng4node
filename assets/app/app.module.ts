e612a88f31ee72b10f131f6b6bce047de29d1112
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-06-29 15:54:44 -0600

    Input, Output

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from  '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { MessageComponent } from './messages/message.component'
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageComponent
     ],
-    imports: [BrowserModule, FormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
