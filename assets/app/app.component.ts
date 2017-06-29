e9bd95832c72e93e0b329e9797f7d90ef835ecf5
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-06-29 14:24:39 -0600

    Created models

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    
+    message = {
+        content: 'A message',
+        author: 'Oswaldo'
+    }
 }
--- routes/app.js
+++ routes/app.js
@@ -3,19 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
-});
-
-router.post('/', function(req, res, next){
-    let email = req.body.email;
-    let user = new User({
-        firstName: 'Oz',
-        lastName: 'Salazar',
-        password: 'super-secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
