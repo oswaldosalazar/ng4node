3b2c0678428828b4d2531c46d04420c758cbca44
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-07-03 13:19:18 -0600

    Login back end completed

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -25,5 +26,35 @@ router.post('/', function (req, res, next) {
   });
 });
 
+router.post('/login', function (req, res, next) {
+  User.findOne({email: req.body.email}, function(err, user){
+    let passworStatus = bcrypt.compareSync(req.body.password, user.password);
+    let token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occurred',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    if (!passworStatus) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    res.status(200).json({
+      message: 'Successfully logged in',
+      token: token,
+      userId: user._id
+    })
+  })
+});
 
 module.exports = router;
